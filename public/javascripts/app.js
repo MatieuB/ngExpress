28a83ab99a55f572ff1f372cbf8c72dac10fa3d0
Matthew Bouchard <bouchard.matthewj@gmail.com>
2016-10-31 09:40:35 -0600

    wired up routes

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,1 +1,20 @@
-angular.module('myApp',[])
+ var app = angular.module('pirates',['ngRoute'])
+
+ app.config(function ($routeProvider, $locationProvider) {
+     $routeProvider
+       .when('/', {
+         templateUrl: '../views/pirates.html',
+         controller: function ($scope) {
+           $scope.msg = 'hello world'
+         }
+       })
+       .when('/login', {
+         templateUrl: 'login',
+         controller: 'LoginCtrl'
+       })
+       .otherwise({
+         redirectTo: '/'
+       });
+
+    //  $locationProvider.html5Mode(true);
+   });
